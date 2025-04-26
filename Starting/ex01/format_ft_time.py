34f67942dbd6982c36fce5ec0ee70b508a83c2b0
zekmaro <zekmaro@yandex.ru>
2025-04-26 14:24:07 +0200

    feat: ex01

---
+++ Starting/ex01/format_ft_time.py
@@ -0,0 +1,16 @@
+import time
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+seconds = time.time()
+
+print(f"Seconds since January 1, 1970: {seconds} or {seconds:.2e} in scientific notation")
+
+current_month = time.localtime(seconds).tm_mon
+current_day = time.localtime(seconds).tm_mday
+current_year = time.localtime(seconds).tm_year
+
+month_name = months[current_month - 1]
+
+print(f"{month_name} {current_day} {current_year}")
