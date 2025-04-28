448ee4f374f573ab8575931d49cd04ad72bd8ec6
zekmaro <zekmaro@yandex.ru>
2025-04-28 12:33:26 +0200

    feat: ex07

---
+++ Starting/ex07/sos.py
@@ -0,0 +1,45 @@
+import sys
+
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...',
+    'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-',
+    'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-', ' ': '/',
+}
+
+
+def main():
+    """
+    Program that takes a string and converts it to Morse code.
+    """
+    if len(sys.argv) != 2:
+        raise AssertionError("Incorrect number of arguments.")
+
+    text = sys.argv[1].upper()
+
+    try:
+        morse_code = ' '.join(MORSE_CODE_DICT[c] for c in text)
+    except KeyError:
+        raise AssertionError("the arguments are bad")
+
+    print(morse_code)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"AssertionError: {e}")
+        sys.exit(1)
