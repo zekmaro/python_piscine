b5fe606cf5c196bdb9281daf45720e6467b9b405
zekmaro <zekmaro@yandex.ru>
2025-04-28 12:02:27 +0200

    refact: ex06

--- Starting/ex06/filterstring.py
+++ Starting/ex06/filterstring.py
@@ -1,32 +1,26 @@
 import sys
-from ft_filter import ft_filter
-
 
 def main():
     """
-    Program that accepts two arguments: a string(S), and an integer(N).
-    Return a list of words from S that have a length greater than N
+    Program that accepts two arguments: a string (S), and an integer (N).
+    Return a list of words from S that have a length greater than N.
     """
-    if (len(sys.argv) != 2):
+    if len(sys.argv) != 3:
         raise AssertionError("Incorrect number of arguments.")
 
     text = sys.argv[1]
-    if type(text) != str:
-        raise AssertionError("Argument is not a string.")
+    try:
+        N = int(sys.argv[2])
+    except ValueError:
+        raise AssertionError("Second argument is not an integer.")
 
-    N = sys.argv[2]
-    if type(N) != int:
-        raise AssertionError("Argument is not an integer.")
     if N < 0:
         raise AssertionError("Argument is negative.")
-    if N > len(text):
-        raise AssertionError("Argument is greater than the string length.")
 
     words = text.split()
-    filtered_words = ft_filter(lambda word: len(word) > N, words)
-    print(list(filtered_words))
+    filtered_words = [word for word in words if (lambda w: len(w) > N)(word)]
+    print(filtered_words)
+
 
 if __name__ == "__main__":
     main()
-
-    
