3b2149e87988a778f2870eb600a6172a772e7a7e
Andrey Arama <zekmaro@yandex.ru>
2025-04-29 12:22:55 +0200

    feat: ex09 done

---
+++ Starting/ex09/ft_package/count_in_list.py
@@ -0,0 +1,3 @@
+def count_in_list(lst, item):
+    """Return how many times item appears in lst."""
+    return lst.count(item)
---
+++ Starting/ex09/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    description="A sample test package",
+    url="https://github.com/eagle/ft_package",
+    author="eagle",
+    author_email="eagle@42.fr",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
