0da4e6b78f1698c84ac06b62ebc2d9b252b00783
Andrey Arama <zekmaro@yandex.ru>
2025-04-29 09:59:24 +0200

    feat: ex08

--- Starting/ex08/Loading.py
+++ Starting/ex08/Loading.py
@@ -1,6 +1,18 @@
-def ft_tqdm(lst: range) -> None:
-    """ft_tqdm(lst: range) -> None
+from time import sleep
+
+
+def ft_tqdm(lst: range):
+    """
     A simple implementation of tqdm to show progress in a loop.
     """
-    for i in range(len(lst)):
-        yield "█"
+    BAR_WIDTH = 128
+    BLOCK_CHAR = '█'
+    length = len(lst)
+    for count, item in enumerate(lst, 1):
+        percent = count / length
+        bar_length = int(percent * BAR_WIDTH)
+        bar = BLOCK_CHAR * bar_length + ' ' * (BAR_WIDTH - bar_length)
+        print(f"\r{int(percent*100)}%|{bar}| {count}/{length}", end="")
+        sleep(0.001)
+        yield item
+    print()
--- Starting/ex08/tester.py
+++ Starting/ex08/tester.py
@@ -3,8 +3,8 @@ from tqdm import tqdm
 from Loading import ft_tqdm
 
 
-# for elem in ft_tqdm(range(333)):
-#     sleep(0.005)
-# print()
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+
 for elem in tqdm(range(333)):
     sleep(0.005)
