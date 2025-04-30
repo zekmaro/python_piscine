038368c691cd91da5fc79d938184831f9c2ce144
Andrey Arama <zekmaro@yandex.ru>
2025-04-30 15:37:49 +0200

    feat: ex02 add doc

--- Array/ex02/load_image.py
+++ Array/ex02/load_image.py
@@ -3,6 +3,9 @@ import numpy as np
 
 
 def ft_load(path: str) -> np.ndarray:
+	"""Load an image from a file and convert it to a numpy array."""
+	if not isinstance(path, str):
+		raise TypeError("path must be a string")
 	try:
 		img = Image.open(path)
 	except FileNotFoundError:
